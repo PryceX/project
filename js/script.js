11ed0bbe36d5dabaaec0e937494ab879c1a5ff63
PryceX <p.kostya4985@gmail.com>
2020-05-17 01:20:35 +0300

    Own

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 "use strict";
 
-let incr = 10,
-    decr = 10;
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", ""),
+    a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", ""),
+    c = prompt("Один из последних просмотренных фильмов?", ""),
+    d = prompt("На сколько оцените его?", "");
 
+let personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(++incr);
-console.log(incr);
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+console.log(personalMovieDb);
